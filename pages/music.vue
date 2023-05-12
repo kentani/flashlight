85e8d944621b13f3b3dd3bacee0f709b6d723fbd
Kento Tani <k.tani0212@gmail.com>
2023-05-12 21:11:15 +0900

    music修正

--- pages/music.vue
+++ pages/music.vue
@@ -34,7 +34,7 @@
 
             <v-card-actions class="pt-0">
               <v-slider
-                v-model="currentTime"
+                v-model="musicList[key]['currentTime']"
                 hide-details
                 min="0"
               ></v-slider>
@@ -47,6 +47,7 @@
 </template>
 
 <script>
+import shu from '@/assets/sounds/33.mp3'
 import sarada from '@/assets/sounds/shiawasenosarada.mp3'
 
 export default {
@@ -54,25 +55,25 @@ export default {
   data () {
     return {
       musicList: {
-        'sarada': { 'title': '幸せのサラダ', 'icon': 'mdi-play' }
+        'sarada': { 'title': '幸せのサラダ', 'icon': 'mdi-play', 'currentTime': 0 },
+        'shu': { 'title': 'シュッ', 'icon': 'mdi-play', 'currentTime': 0 }
       },
       playingMusic: '',
-      audio: {},
-      currentTime: 0
+      audio: {}
     }
   },
   mounted() {
-    // const music = this.fetchMusic(selectedMusic);
-    this.audio = new Audio(sarada);
   },
   methods: {
     onClickMusicBtn(selectedMusic) {
       if (this.playingMusic === selectedMusic) {
         this.audio.pause();
-        this.currentTime = this.audio.currentTime;
         this.playingMusic = '';
+        this.musicList[selectedMusic]['currentTime'] = this.audio.currentTime;
         this.musicList[selectedMusic]['icon'] = 'mdi-play';
       } else {
+        const music = this.fetchMusic(selectedMusic);
+        this.audio = new Audio(music);
         this.audio.play();
         this.playingMusic = selectedMusic;
         this.musicList[selectedMusic]['icon'] = 'mdi-pause';
@@ -82,6 +83,8 @@ export default {
       switch (selectedMusic) {
         case 'sarada':
           return sarada;
+        case 'shu':
+          return shu;
       }
     }
   }
