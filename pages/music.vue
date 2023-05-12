24d19eb8b668ff84740f22e9b6144f7b62c75bef
Kento Tani <k.tani0212@gmail.com>
2023-05-12 16:53:21 +0900

    music修正

--- pages/music.vue
+++ pages/music.vue
@@ -6,7 +6,7 @@
     style="height: 100%; width: 100%;"
   >
     <v-col
-      v-for="music in musicList"
+      v-for="(music, key) in musicList"
       cols="9"
       md="3"
       lg="2"
@@ -20,12 +20,21 @@
             rounded="xl"
             :elevation="hover? 24 : 8"
             nuxt
-            @click="onClickPlay(music.music)"
           >
-            <v-card-text class="text-body-1 font-weight-bold text-center">
-              <v-icon size="150">mdi-play</v-icon>
+            <v-card-text class="text-body-1 font-weight-bold text-center pb-0" @click="onClickMusicBtn(key)">
+              <v-icon size="150">
+                {{ music.icon }}
+              </v-icon>
               <p class="mb-0">{{ music.title }}</p>
             </v-card-text>
+
+            <v-card-actions class="pt-0">
+              <v-slider
+                hide-details
+                max="50"
+                min="-50"
+              ></v-slider>
+            </v-card-actions>
           </v-card>
         </template>
       </v-hover>
@@ -40,19 +49,29 @@ export default {
   name: 'music',
   data () {
     return {
-      musicList: [
-        { 'music': 'sarada', 'title': '幸せのサラダ'  }
-      ]
+      musicList: {
+        'sarada': { 'title': '幸せのサラダ', 'icon': 'mdi-play' }
+      },
+      playingMusic: ''
     }
   },
   mounted () {
   },
   methods: {
-    onClickPlay(selectedMusic) {
+    onClickMusicBtn(selectedMusic) {
       const music = this.fetchMusic(selectedMusic);
       const audio = new Audio(music);
-      audio.currentTime = 0;
-      audio.play();
+
+      if (this.playingMusic === selectedMusic) {
+        audio.pause();
+        this.playingMusic = '';
+        this.musicList[selectedMusic]['icon'] = 'mdi-play';
+      } else {
+        audio.currentTime = 0;
+        audio.play();
+        this.playingMusic = selectedMusic;
+        this.musicList[selectedMusic]['icon'] = 'mdi-pause';
+      }
     },
     fetchMusic(selectedMusic) {
       switch (selectedMusic) {
