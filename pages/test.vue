5ddc582c8c37aa7ca12aa735d0b3a05e9da792a1
Kento Tani <k.tani0212@gmail.com>
2023-06-01 15:21:53 +0900

    pwa

---
+++ pages/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container class="my-6">
+    <v-card color="#26c6da" width="520" class="my-6">
+      <v-icon dark size="512">mdi-teddy-bear</v-icon>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TestPage',
+  components: {
+  },
+  data: () => ({
+    image: require('@/assets/images/teddy-bear.png'),
+  }),
+  async mounted() {
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+</style>
